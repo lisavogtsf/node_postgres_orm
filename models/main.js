865ddba26f359e5a055b39080b698fc3fd8bacd5
Lisa Vogt <lisavogt.sf@gmail.com>
2014-07-25 17:28:05 -0700

    stuff

--- models/main.js
+++ models/main.js
@@ -4,41 +4,46 @@ var Models = {};
 
 Models.Person = Person;
 
-var noob = {firstname: "Windsay", lastname: "Wohan"};
-Models.Person.create(noob, function(err, noobPerson){
-console.log(noobPerson);
-});
+// // create function--working
+// var noob = {firstname: "Windsay", lastname: "Wohan"};
+// Models.Person.create(noob, function(err, noobPerson){
+// console.log(noobPerson);
+// });
 
 
-// working all function
+// // all function--working 
 // Models.Person.all(function(err, people){
 //   // logs all database elements
 //   console.log(people);
 // });
 
-// non-functioning findBy
-// Models.Person.findBy('firstname', 'Mary', function(err, foundPerson){
+// // findBy function--working
+// Models.Person.findBy('firstname', 'Blim', function(err, foundPerson){
 //   console.log("Found: ", foundPerson);
-//   // console.log("query is: WHERE " + key + "=$1",[val]);
 // });
 
-
-
-
+// update function--working
 // findby and update
-// Models.Person.findBy("id", 32, function(err, person){
+// find passes a foundPerson object into update
+// then update sets 
+// Models.Person.findBy("id", 4, function(err, person){
 //   console.log("found", person);
-//   person.update({firstname: "sam", lastname: "creek"}, function(err, person){
+//   person.update({firstname: "Sam", lastname: "Spade"}, function(err, person){
 //     console.log("UPDATED:", person)
 //   });
 // })
 
+// // destroy function--not working
+// // original function
+// Person.prototype.destroy = function(){
+//   db.query("", [this.id], function(err, res) {
+//     callback(err)
+//   });
+// }
 
 
-// Person.prototype.update = function(params, callback){ id: 32,
-//     firstname: 'Lala Lala',
-//     lastname: 'Walla Walla',
-//     created_at: Thu Jul 24 2014 19:38:54 GMT-0700 (PDT) };
+
+
 
 
 
